a66cbce070ae36a0940cccf129bc09615536761a
Sciencetrooper <mehta.paritosh95@gmail.com>
2016-03-24 15:51:10 +0530

    extract redundance and prepare for deployment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,28 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 def index
   @articles = Article.all
 end
 
-
-  def new
-    @article = Article.new
-  end
-
-def edit
-  @article = Article.find(params[:id])
+def new
+  @article = Article.new
 end
 
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      flash[:notice] = 'Article was successfully created'
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
+def edit
+end
+
+def create
+  @article = Article.new(article_params)
+  if @article.save
+    flash[:notice] = 'Article was successfully created'
+    redirect_to article_path(@article)
+  else
+    render 'new'
   end
+end
 
 def update
-  @article = Article.find(params[:id])
   if @article.update(article_params)
     flash[:notice] = 'Article was Successfuly Updated'
     redirect_to article_path(@article)
@@ -34,19 +32,19 @@ def update
 end
 
 def show
-  @article = Article.find(params[:id])
 end
 
 def destroy
-  @article = Article.find(params[:id])
   @article.destroy
   flash[:notice] = "Article was successfully deleted"
   redirect_to articles_path
 end
 
 private
+  def set_article
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
 end
